3d5ca62fb53354919bd45d0819057c676fe4b288
leerosales <leerosales@gmail.com>
2020-06-15 18:57:22 -0500

    Actualizar archivo

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Este paquete es creado para gestionar los métodos de las operaciones de venta un canal.
+ * Este paquete es creado para gestionar los 
+ * métodos de las operaciones de venta un canal.
  */
  package com.example;
