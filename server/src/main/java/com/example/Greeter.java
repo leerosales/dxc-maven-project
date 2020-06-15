58d4168a230d4acb5d220e3c05f930679d0cc87c
leerosales <leerosales@gmail.com>
2020-06-15 18:51:38 -0500

    Actualizacion

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param someone Nombre de un empleado
+   * @return Cadena tipo String
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
